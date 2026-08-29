3b1945b83828c0cb7a03ef3dd69353f2ed87a6e0
AkifNaveed12 <akif27031@gmail.com>
2026-08-29 21:41:25 +0500

    routing completed

--- crispyTribble/src/App.jsx
+++ crispyTribble/src/App.jsx
@@ -1,11 +1,28 @@
 import { useState } from 'react'
 import './App.css'
+import {Routes, Route} from 'react-router-dom'
+import Navbar from './components/navbar/Navbar'
+import Home from './pages/home/Home'
+import Details from './pages/details/Details'
+import Favorite from './pages/favorites/Favorites'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div>hello akif</div>
+    <div>
+      <div className='min-h-screen p-6 bg-white text-gray-600 text-lg'>
+        <Navbar />
+        <Routes>
+          
+          {/* page routes */}
+          <Route path ='/' element={ <Home/> }/>
+          <Route path ='/favorites' element={ <Favorite/> }/>
+          <Route path ='/recipe-item/:id' element={ <Details/> }/>
+
+        </Routes>
+      </div>
+    </div>
   )
 }
 
--- crispyTribble/src/main.jsx
+++ crispyTribble/src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import {BrowserRouter} from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  // wrapping the app in browser router wrapper
+  <BrowserRouter>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </BrowserRouter>
 )
