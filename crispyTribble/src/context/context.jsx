e991b85e7db6aa3f3ae6282998858d738c454200
AkifNaveed12 <akif27031@gmail.com>
2026-09-01 13:54:54 +0500

    recipe details page image added and bugs fixed

--- crispyTribble/src/context/context.jsx
+++ crispyTribble/src/context/context.jsx
@@ -2,36 +2,47 @@ import { createContext, useState } from "react";
 
 export const GlobalContext = createContext(null);
 
+export default function GlobalState({ children }) {
+  const [searchParam, setSearchParam] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [recipeList, setRecipeList] = useState([]);
+  const [recipeDetailsData, setRecipeDetailsData] = useState(null);
 
-export default function GlobalState({children}){
+  async function handleSubmit(event) {
+    event.preventDefault();
+    try {
+        const res = await fetch(
+            `https://forkify-api.jonas.io/api/v2/recipes?search=${searchParam}`,
+        );
+        const data = await res.json();
 
-    const [searchParam, setSearchParam] = useState('');
-    const[loading, setLoading] = useState(false);
-    const[recipeList, setRecipeList] = useState([]);
-
-
-    async function handleSubmit(event) {
-        event.preventDefault();
-        try {
-            
-            const res = await fetch(`https://forkify-api.jonas.io/api/v2/recipes?search=${searchParam}`);
-            const data = await res.json();
-
-            if(data?.data?.recipes){
-                setRecipeList(data?.data?.recipes);
-                setLoading(false);
-                setSearchParam('');
-            }
-        } catch (e) {
-            console.log(e);
+        if (data?.data?.recipes) {
+            setRecipeList(data?.data?.recipes);
             setLoading(false);
-            setSearchParam('');
-        }
+            setSearchParam("");
     }
+    } catch (e) {
+        console.log(e);
+        setLoading(false);
+        setSearchParam("");
+    }
+}
 
-    console.log(loading, recipeList);
+console.log(loading, recipeList);
 
-    return <GlobalContext.Provider value={{ searchParam, setSearchParam, handleSubmit, loading, recipeList }}>
+return (
+    <GlobalContext.Provider
+        value={{
+            searchParam,
+            setSearchParam,
+            handleSubmit,
+            loading,
+            recipeList,
+            recipeDetailsData,
+            setRecipeDetailsData,
+    }}
+    >
         {children}
     </GlobalContext.Provider>
+);
 }
--- crispyTribble/src/pages/details/Details.jsx
+++ crispyTribble/src/pages/details/Details.jsx
@@ -1,13 +1,44 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useContext, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { GlobalContext } from "../../context/context";
 
 const Details = () => {
+    const { id } = useParams();
+    const { recipeDetailsData, setRecipeDetailsData } = useContext(GlobalContext);
 
-    const params = useParams();
-    console.log(params);
-    return (
-        <div>Details</div>
+useEffect(() => {
+    async function getRecipeDetails() {
+        const response = await fetch(
+        `https://forkify-api.jonas.io/api/v2/recipes/${id}`
     );
-}
+    const data = await response.json();
+
+    console.log(data);
+    console.log(data?.data?.recipe?.image_url);
+    
+    if (data?.data) {
+        setRecipeDetailsData(data?.data);
+    }
+    }
+    getRecipeDetails();
+}, [id]);
+
+
+    console.log("STATE:", recipeDetailsData);
+    console.log("IMAGE:", recipeDetailsData?.recipe?.image_url);
+
+return (
+    <div className="container mx-auto py-10 grid grid-cols-1 lg:grid-cols-2 gap-10">
+        <div className="row-start-2 lg:row-start-auto">
+        <div className="h-96 overflow-hidden rounded-xl group">
+            <img
+            src={recipeDetailsData?.recipe?.image_url}
+            className="w-full h-full group-hover:scale-105 duration-300"
+        />
+        </div>
+    </div>
+    </div>
+);
+};
 
 export default Details;
